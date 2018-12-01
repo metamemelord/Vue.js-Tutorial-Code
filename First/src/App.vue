941d985055aadc9caa8e6b1ce81417227b20dbe5
metamemelord <sainigaurav.2009@gmail.com>
2018-12-01 21:33:00 +0530

    Added codes

---
+++ First/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Server Status: {{status}}</h1>
+    <app-servers v-for="server in 5"></app-servers>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return { status: "Normal" };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ First/src/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <app-server-status>
+    </app-server-status>
+</template>
+
+<script>
+    import serverStatus from './ServerStatus.vue';
+export default {
+    components: {
+        'app-server-status': serverStatus
+    }
+}
+</script>
