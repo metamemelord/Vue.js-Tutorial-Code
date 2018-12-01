a7e1faedb0429630e2306aec6efdecdb382431ec
metamemelord <sainigaurav.2009@gmail.com>
2018-12-01 22:29:35 +0530

    Added more projects

---
+++ Third - Assignment/src/components/Middle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <servers-list></servers-list>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ServersList from "./ServersList.vue";
+
+export default {
+  components: {
+    "servers-list": ServersList
+  }
+};
+</script>
